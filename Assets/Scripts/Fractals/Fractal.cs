4dfc8053c30b3b233013c55f4b5ee3d8ce792969
sheepp91 <joshua_laurence@live.com>
2017-06-26 23:24:44 +1000

    Fractals Tutorial complete

---
+++ Assets/Scripts/Fractals/Fractal.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour {
+
+    //public Mesh mesh;
+    public Mesh[] meshes;
+    public Material material;
+    public int maxDepth;
+    public float childScale;
+    public float spawnProbability;
+
+    private int depth;
+    private Vector3[] directions = {
+        Vector3.up,
+        Vector3.left,
+        Vector3.right,
+        Vector3.forward,
+        Vector3.back
+    };
+    private Quaternion[] orientation = {
+        Quaternion.identity,
+        Quaternion.Euler(0, 0, -90f),
+        Quaternion.Euler(0, 0, 90f),
+        Quaternion.Euler(90, 0, 0),
+        Quaternion.Euler(-90, 0, 0)
+    };
+
+    private Material[,] materials;
+
+    public void InitialiseMaterials() {
+        materials = new Material[maxDepth + 1, 2];
+        for (int i = 0; i <= maxDepth; i++) {
+            float t = i / (maxDepth - 1f);
+            t *= t;
+            materials[i, 0] = new Material(material);
+            materials[i, 0].color = Color.Lerp(Color.white, Color.yellow, t);
+            materials[i, 1] = new Material(material);
+            materials[i, 1].color = Color.Lerp(Color.white, Color.cyan, t);
+
+        }
+        materials[maxDepth, 0].color = Color.magenta;
+        materials[maxDepth, 1].color = Color.red;
+    }
+
+    void Start () {
+        if (materials == null) {
+            InitialiseMaterials();
+        }
+        gameObject.AddComponent<MeshFilter>().mesh = meshes[Random.Range(0, meshes.Length)];
+        gameObject.AddComponent<MeshRenderer>().material = material;
+        GetComponent<MeshRenderer>().material = materials[depth, Random.Range(0, 2)];
+        if (depth < maxDepth) {
+            StartCoroutine(createChildren());
+        }
+	}
+
+    private IEnumerator createChildren() {
+        for (int i = 0; i < directions.Length; i++) {
+            if (Random.value < spawnProbability) {
+                yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+                new GameObject("Fractal Child").AddComponent<Fractal>().Initialise(this, i);
+            }
+        }
+    }
+
+    private void Initialise(Fractal parent, int index) {
+        meshes = parent.meshes;
+        materials = parent.materials;
+        maxDepth = parent.maxDepth;
+        depth = parent.depth + 1;
+        spawnProbability = parent.spawnProbability;
+        childScale = parent.childScale;
+        transform.parent = parent.transform;
+        transform.localScale = Vector3.one * childScale;
+        transform.localPosition = directions[index] * (0.5f + 0.5f * childScale);
+        transform.localRotation = orientation[index];
+    }
+}
