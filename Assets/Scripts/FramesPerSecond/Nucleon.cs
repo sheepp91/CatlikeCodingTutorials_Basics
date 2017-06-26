c3ffeb3778a9f0c3659afc4d5ec6721f6afb7c40
sheepp91 <joshua_laurence@live.com>
2017-06-27 00:16:49 +1000

    Almost finished Frames Per Second

---
+++ Assets/Scripts/FramesPerSecond/Nucleon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour {
+
+    public float attractionForce;
+
+    private Rigidbody rb;
+
+	void Awake () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+        rb.AddForce(transform.localPosition * -attractionForce);
+	}
+}
