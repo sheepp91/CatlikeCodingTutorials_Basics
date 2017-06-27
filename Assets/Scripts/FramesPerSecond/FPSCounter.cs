c4bf6a704bf8c24c9ff025929812248a053f0260
sheepp91 <joshua_laurence@live.com>
2017-06-27 23:17:04 +1000

    FPS Continued

---
+++ Assets/Scripts/FramesPerSecond/FPSCounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour {
+    public int AverageFPS { get;  private set; }
+    public int frameRange = 60;
+
+    private int[] fpsBuffer;
+    private int fpsBufferIndex;
+
+    void Update () {
+        AverageFPS = (int)(1f / Time.unscaledDeltaTime);
+	}
+
+    void InitializeBuffer() {
+        if (frameRange <= 0) {
+            frameRange = 1;
+        }
+        fpsBuffer = new int[frameRange];
+        fpsBufferIndex = 0;
+    }
+}
